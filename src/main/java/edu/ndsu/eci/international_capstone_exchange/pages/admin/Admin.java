49058b18fdf00e346fd13be436c5680f04141900
TimHunter <tim.m.hunter@ndsu.edu>
2018-12-26 14:37:59 -0600

    Updated "Set Logger Level" system to more closely match the EAD-Admin project's example.

--- src/main/java/edu/ndsu/eci/international_capstone_exchange/pages/admin/Admin.java
+++ src/main/java/edu/ndsu/eci/international_capstone_exchange/pages/admin/Admin.java
@@ -13,17 +13,14 @@
 // limitations under the License.
 package edu.ndsu.eci.international_capstone_exchange.pages.admin;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.cayenne.ObjectContext;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.tapestry5.ValueEncoder;
 import org.apache.tapestry5.annotations.Import;
-import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
@@ -51,16 +48,45 @@ public class Admin {
   @Property
   private int userCount;
   
-  /** Holds log level selection. */
-  @Property
-  private String levelSelect;
-  
-  /** Logger reference */
-  private static final Logger LOGGER = Logger.getLogger(Admin.class);
-  
-  
   /** Database map reference */
   private CapstoneDomainMap map = CapstoneDomainMap.getInstance();
+  
+  
+  /** Tapestry package used in logger */
+  private static final String TAPESTRY_PACKAGE = "org.apache.tapestry";
+
+  /** NDSU library package used in logger */
+  private static final String NDSU_PACKAGE = "edu.ndsu";
+
+  /** Cayenne package used in logger */
+  private static final String CAYENNE_PACKAGE = "org.apache.cayenne";
+  
+  /** Project package used in logger */
+  private static final String APPLICATION_PACKAGE = "ndsu.eci.international_capstone_exchange";
+  
+  @Property
+  private List<Level> allLevels; 
+  
+  /** Holds root level selection. */
+  @Property
+  private Level rootLevel;
+  
+  /** Holds root level selection. */
+  @Property
+  private Level applicationLevel;
+  
+  /** Holds cayenne level selection. */
+  @Property
+  private Level cayenneLevel;
+  
+  /** Holds root level selection. */
+  @Property
+  private Level ndsuLevel;
+  
+  /** Holds root level selection. */
+  @Property
+  private Level tapestryLevel;
+  
   
   /**
    * Setup Render used to get pending task information.
@@ -68,48 +94,95 @@ public class Admin {
   public void setupRender() {
     proposalCount = map.performProposalsByStatus(context, ProposalStatus.PENDING).size();
     userCount = map.performUsersByStatus(context, Status.PENDING).size();
+    
+    allLevels = new ArrayList<>();
+    allLevels.add(Level.ALL);
+    allLevels.add(Level.DEBUG);
+    allLevels.add(Level.ERROR);
+    allLevels.add(Level.FATAL);
+    allLevels.add(Level.INFO);
+    allLevels.add(Level.OFF);
+    allLevels.add(Level.TRACE);
+    allLevels.add(Level.WARN);
+    
+    rootLevel = LogManager.getRootLogger().getLevel();
+    applicationLevel = LogManager.getLogger(APPLICATION_PACKAGE).getEffectiveLevel();
+    cayenneLevel = LogManager.getLogger(CAYENNE_PACKAGE).getEffectiveLevel();
+    ndsuLevel = LogManager.getLogger(NDSU_PACKAGE).getEffectiveLevel();
+    tapestryLevel = LogManager.getLogger(TAPESTRY_PACKAGE).getEffectiveLevel();
   }
   
   /**
-   * Get value of levelSelect.
+   * Change log level as appropriate.
    */
-  public void onActivate(String value) {
-    this.levelSelect = value;
+  public void onSuccessFromRoot() {
+    LogManager.getRootLogger().setLevel(rootLevel);
   }
   
   /**
-   * Set submission value of levelSelect.
-   * @return Value of levelSelect.
+   * Change log level as appropriate.
    */
-  public String onPassivate() {
-    return levelSelect;
+  public void onSuccessFromApplication() {
+    LogManager.getLogger(APPLICATION_PACKAGE).setLevel(applicationLevel);
   }
   
   /**
-   * On Success of Log Form submission, change log level as appropriate.
-   * Based off this example: http://www.mooreds.com/wordpress/archives/238
+   * Change log level as appropriate.
    */
-  public void onSuccess() {
-    //Changes root logger settings, the easy way
-    //LogManager.getRootLogger().setLevel(Level.toLevel(levelSelect));
-    
-    //Get log4j properties file for runtime changes
-    Properties properties = new Properties();
-    try {
-      InputStream stream = getClass().getResourceAsStream("/log4j.properties");
-      properties.load(stream);
-      stream.close();
-    } catch(IOException ioe) {
-      LOGGER.warn("Could not load log4j properties file.", ioe);
-    }
-    
-    //Specify which settings should be changed
-    properties.setProperty("log4j.rootCategory", levelSelect + ", A1");
-    //properties.setProperty("log4j.category.edu.ndsu.eci.international_capstone_exchange.auth.LocalDevRealm", levelSelect);
-    //properties.setProperty("log4j.logger.org.apache.cayenne.access.QueryLogger", levelSelect);
-    
-    //Apply config changes
-    LogManager.resetConfiguration();
-    PropertyConfigurator.configure(properties);
+  public void onSuccessFromCayenne() {
+    LogManager.getLogger(CAYENNE_PACKAGE).setLevel(cayenneLevel);
   }
+  
+  /**
+   * Change log level as appropriate.
+   */
+  public void onSuccessFromNdsu() {
+    LogManager.getLogger(NDSU_PACKAGE).setLevel(ndsuLevel);
+  }
+  
+  /**
+   * Change log level as appropriate.
+   */
+  public void onSuccessFromTapestry() {
+    LogManager.getLogger(TAPESTRY_PACKAGE).setLevel(tapestryLevel);
+  }
+  
+  /**
+   * Encoder used to translate between selection Strings and selected Level value.
+   * @return Encoder for Level translation.
+   */
+  public ValueEncoder<Level> getLevelEncoder() {
+    return new ValueEncoder<Level>() {
+
+      @Override
+      public String toClient(Level value) {
+        return value.toString();
+      }
+
+      @Override
+      public Level toValue(String clientValue) {
+        switch(clientValue) {
+        case "ALL":
+          return Level.ALL;
+        case "DEBUG":
+          return Level.DEBUG;
+        case "ERROR":
+          return Level.ERROR;
+        case "FATAL":
+          return Level.FATAL;
+        case "INFO":
+          return Level.INFO;
+        case "OFF":
+          return Level.OFF;
+        case "TRACE":
+          return Level.TRACE;
+        case "WARN":
+          return Level.WARN;
+        default:
+          return null;
+        }
+      }
+    };
+  }
+  
 }
